64a53ec7f7e5593b1e91de0c0e2ad3e3400774dc
miracleonyenma <miracleficient@gmail.com>
2023-01-30 16:14:50 +0100

    init: search with array of strings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// ./src/App.js
+
+import { useEffect, useState } from "react";
+import Fuse from "fuse.js";
+
+const bookTitles = [
+  "Mercy's Birds",
+  "The Last of the Mohicans",
+  "Harry Potter and the Sorcerer's Stone",
+  "The Hobbit",
+  "The Lord of the Rings",
+  "The Fellowship of the Ring",
+  "The Two Towers",
+  "The Sandman",
+  "The Return of the King",
+];
 
 function App() {
+  // search term
+  const [searchTerm, setSearchTerm] = useState(" ");
+
+  // options object for fuse.js
+  const options = {
+    includeScore: true,
+  };
+
+  // initialize fuse with list and options
+  const fuse = new Fuse(bookTitles, options);
+
+  // search based on search term entered
+  const searchResults = fuse.search(searchTerm);
+
+  // function to update search term from input element
+  const handleSearch = ({ currentTarget }) => {
+    const { value } = currentTarget;
+    setSearchTerm(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="p-4">
+        <input
+          value={searchTerm}
+          onChange={handleSearch}
+          type="text"
+          placeholder="Search"
+          className="bg-slate-200 p-2 rounded"
+        />
+
+        <ul className="results">
+          {searchResults.map((result) => (
+            <li className="result my-4">
+              <h2>{result.item}</h2>
+              <span>Score: {result.score}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
 
